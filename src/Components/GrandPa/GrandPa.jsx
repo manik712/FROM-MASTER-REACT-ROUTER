6064f8f888b559947e57e54b67c3ea7cdccd2917
manik712 <ranineela71@gmail.com>
2023-10-01 08:34:05 -0700

    add all

--- src/Components/GrandPa/GrandPa.jsx
+++ src/Components/GrandPa/GrandPa.jsx
@@ -1,20 +1,33 @@
+import { createContext } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import  "./GrandPa.css"
 
+const AssetContext = createContext("gold")
 const GrandPa = () => {
   const asset = "diamond"
   return (
     <div className="GrandPa flex">
       <h2>GrandPa</h2>
-       <section>
+      <AssetContext.Provider value="gold">
+      <section>
        <Dad asset={asset}></Dad>
        <Uncle></Uncle>
        <Aunty></Aunty>
        </section>
+      </AssetContext.Provider>
     </div>
   );
 };
 
 export default GrandPa;
+
+
+
+
+
+// 1.create a context and add export it.
+//2.add the provider for the context WITH the value.
+
+
--- src/Components/RefForm/RefForm.jsx
+++ src/Components/RefForm/RefForm.jsx
@@ -1,32 +1,54 @@
 import { useEffect, useRef } from "react";
 
 const RefForm = () => {
-  const nameRef = useRef() ;
-  const emailRef =useRef();
+  const nameRef = useRef();
+  const emailRef = useRef();
   const passRef = useRef();
-  useEffect(() =>{
-    nameRef.current.focus();
-  })
 
-  const handleSubmit = e =>{
-   e.preventDefault();
-   console.log(nameRef.current.value);
-   console.log(emailRef.current.value);
-   console.log(passRef.current.value);
-  }
+  useEffect(() => {
+    nameRef.current.focus();
+    
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(nameRef.current.value);
+    console.log(emailRef.current.value);
+    console.log(passRef.current.value);
+
+  };
   return (
     <div>
-       <form  className="space-y-2"  onSubmit={handleSubmit}>
-          <input type="text" ref={nameRef}   name="name"  placeholder="name" className="bg-red-500 text-white px-8 py-2 rounded-lg decoration-none"  />
-         <br />
-         <input type="email"  ref={emailRef} placeholder="email"  defaultValue={'maniks@fssdfg.com'} className="bg-red-500 text-white px-8 py-2 rounded-lg decoration-none"  name="email" id="" />
-           <br />
-           <input type="password"  ref={passRef} name="password" placeholder="password" className="bg-red-500 text-white px-8 py-2 rounded-lg decoration-none" id="" />
-           <br />
-         <input type="submit" value="Submit" />
-        </form>
-
-
+      <form className="space-y-2" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          ref={nameRef}
+          name="name"
+          placeholder="name"
+          className="bg-red-500 text-white px-8 py-2 rounded-lg decoration-none"
+        />
+        <br />
+        <input
+          type="email"
+          ref={emailRef}
+          placeholder="email"
+          defaultValue={"maniks@fssdfg.com"}
+          className="bg-red-500 text-white px-8 py-2 rounded-lg decoration-none"
+          name="email"
+          id=""
+        />
+        <br />
+        <input
+          type="password"
+          ref={passRef}
+          name="password"
+          placeholder="password"
+          className="bg-red-500 text-white px-8 py-2 rounded-lg decoration-none"
+          id=""
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 };
